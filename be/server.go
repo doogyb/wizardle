e8fea43b15d3eca01294f739e9614b73b232bf27
doogyb <samueldoogan@gmail.com>
2025-01-06 19:18:40 +0000

    feat: add random-quote endpoint (#6)
    closes #4

--- be/server.go
+++ be/server.go
@@ -1,14 +1,27 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
 
+	"math/rand"
+
+	"github.com/gin-gonic/gin"
+)
+
+var quotes []Quote
+
+func getRandomQuote(c *gin.Context) {
+	c.IndentedJSON(200, quotes[rand.Intn(len(quotes))])
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+
+	quotes = loadQuotes()
+	fmt.Println(quotes)
+
+	router := gin.Default()
+	router.GET("/random-quote", getRandomQuote)
+
+	router.Run()
+
 }
